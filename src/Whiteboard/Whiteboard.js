b89e5dda101af0641ef0afab51ec193eea892a4e
veetmoradiya <veetmoradiya3628@gmail.com>
2024-04-11 21:32:42 +0530

    initial setup ready

---
+++ src/Whiteboard/Whiteboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Menu from "./Menu";
+
+const Whiteboard = () => {
+    return (
+        <>
+            <Menu />
+        </>
+    )
+}
+
+export default Whiteboard;
