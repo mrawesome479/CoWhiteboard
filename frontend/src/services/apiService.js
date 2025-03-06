4bc718697c2239127ac9beb13c747efd41e323bf
mrawesome479 <157299334+mrawesome479@users.noreply.github.com>
2025-03-07 00:52:12 +0530

    Update apiService.js

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5002';
+const API_URL = 'https://cowhiteboard.onrender.com';
 
 export const registerUser = async (reqData) => {
   try{
